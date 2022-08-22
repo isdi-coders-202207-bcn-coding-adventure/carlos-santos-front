8f6a50daad1a0247cc4d13445f96b9b53a525740
Carlos <casantosmu@gmail.com>
2022-08-22 13:46:37 +0200

    Added counter html

---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,24 @@
+const Counter = () => {
+  return (
+    <article>
+      <p>
+        <div>
+          <span>02</span> days
+        </div>
+        <div>
+          <span>20</span> hours
+        </div>
+        <div>
+          <span>34</span> minutes
+        </div>
+        <div>
+          <span>09</span> seconds
+        </div>
+      </p>
+    </article>
+  );
+};
+
+export default Counter;
+
+// 02 days 20 hours 34 minutes 09 seconds
