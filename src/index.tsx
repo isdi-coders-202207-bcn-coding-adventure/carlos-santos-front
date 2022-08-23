83363c06d691498511d385fd024a9b21f8301dba
Carlos <casantosmu@gmail.com>
2022-08-23 12:51:30 +0200

    Added global styles && Removed css styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import "@fontsource/roboto";
 import { store } from "./store/store";
 import App from "./App";
-import "./index.css";
+import GlobalStyle from "./theme/GlobalStyle";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -12,6 +11,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyle />
       <App />
     </Provider>
   </React.StrictMode>
